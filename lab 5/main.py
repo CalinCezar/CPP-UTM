0dc5196cd1d63cd17cb78a2235f11b9522c7ea43
CalinCezar <calincezar2@gmail.com>
2025-11-17 17:10:37 +0200

    lab 5 initial commit

---
+++ lab 5/main.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+gecko_driver = BASE_DIR / "geckodriver-v0.36.0-linux64" / "geckodriver"
+
+options = Options()
+options.binary_location = "/usr/bin/firefox"
+service = Service(executable_path=gecko_driver)
+
+driver = webdriver.Firefox(service=service, options=options)
+
+
+driver.get("https://ebay.com")
+
+search_box = WebDriverWait(driver, 15).until(
+    EC.visibility_of_element_located((By.ID, "gh-ac"))
+)
+search_box.send_keys("computer")
+search_box.send_keys(Keys.ENTER)
+
+WebDriverWait(driver, 15).until(
+    EC.visibility_of_element_located((By.ID, "gh-logo"))
+)
